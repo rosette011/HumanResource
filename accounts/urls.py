a598a1aa23d8c4e455a325d1be19506545fdab85
Rosette <rosette01107@gmail.com>
2018-09-04 22:13:32 +0800

    register

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ app_name = 'accounts'
 
 urlpatterns = [
 
-    path('login', views.index, name="login"),
+    path('login', views.login, name="login"),
     path('register', views.register, name="register"),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,19 @@
 from django.urls import reverse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
-def index(request):
+def login(request):
     return render(request, 'registration/login.html')
 
 def register(request):
 
-    # if request.method == 'POST':
-    #     form = UserCreationForm(request.POST)
-
-    #     if form.is_valid():
-    #         form.save()
-    #         username = form.cleaned_data['username']
-    #         password = form.cleaned_data['password1']
-    #         user = authenticate(username=username,password=password)
-    #         login(request, user)
-
-    form = UserCreationForm()
-    context = {'form':form}
-    return render(request,'registration/register.html',context)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/human_resource/home')
+    else:
+        form = UserCreationForm()
+        context = {'form':form}
+        return render(request,'registration/register.html',context)
