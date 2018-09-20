e38f35bc9b684670029fb459f297d5939040008e
Rosette <rosette01107@gmail.com>
2018-09-20 21:09:22 +0800

    working path for reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,11 +9,27 @@ urlpatterns = [
     path('login', views.login_view, name="login"),
     path('logout', views.logout_view, name="logout"),
     path('register', views.register, name="register"),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='registration/reset-password.html'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='registration/reset-password-done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
     path('profile/', views.profile_view, name="profile_view"),
     path('profile/edit', views.profile_edit, name="profile_edit"),
     path('profile/change-password', views.change_password, name="change_password"),
-    path('reset-password/', auth_views.password_reset , {'template_name':'registration/reset-password.html'},name='password_reset'),
-    path('reset-password/done', auth_views.password_reset_done , {'template_name':'registration/reset-password-done.html'},name='password_reset_done'),    
-    path('reset/<uidb64>/<token>/', auth_views.reset , {'template_name':'registration/reset-password-done.html'},name='password_reset_done'),    
-
 ]
